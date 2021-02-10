13fc76bb17f228ff37e60fc62a03ffe0bea60adc
gnanendra1 <gnanendramgr@gmail.com>
2021-02-10 10:26:57 +0530

    stash cmd

--- CustomerList/src/com/app/cust/dao/CustomerDao.java
+++ CustomerList/src/com/app/cust/dao/CustomerDao.java
@@ -4,6 +4,7 @@ import com.app.cust.model.Customer;
 
 public interface CustomerDao {
 	public Customer addCustomer(Customer cutomer);
-	
+	public Customer show(int cutomerId);
+	public Customer removeCustomer(int cutomerId);	
 
 }
