dc116b6c87d32eef16818c38270a08c8c402d992
gnanendra1 <gnanendramgr@gmail.com>
2021-02-10 09:38:32 +0530

    dao interface added show and remove method

--- CustomerList/src/com/app/cust/dao/CustomerDao.java
+++ CustomerList/src/com/app/cust/dao/CustomerDao.java
@@ -4,5 +4,7 @@ import com.app.cust.model.Customer;
 
 public interface CustomerDao {
 	public Customer addCustomer(Customer cutomer);
+	public Customer showCustomer(int custId);
+	public Customer removeCustomer(int custId);
 
 }
